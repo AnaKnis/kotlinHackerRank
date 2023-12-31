16f17d941eaf5f6275375ebd00b8d81bc7f2a20d
Ana Júlia Knischewski <ana.julia@kxptech.com>
2023-12-31 13:13:43 -0300

    add: solve me first

---
+++ src/solveMeFirst.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(){
+    val sc = Scanner(System.`in`);
+    val num1 = sc.nextInt();
+    val num2 = sc.nextInt();
+
+    val sum = num1 + num2;
+    println(sum);
+}
